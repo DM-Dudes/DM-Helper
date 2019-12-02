968abb33c7b093b355775aadb93116fc5f22e74c
olj1 <john.losier1@gmail.com>
2019-12-02 12:54:22 -0600

    DMTable model, views, and serializer created, runs on local server

--- DM_helper_backend/dm_app/models.py
+++ DM_helper_backend/dm_app/models.py
@@ -7,3 +7,9 @@ class User(models.Model):
     def __str__(self):
         return self.name
         
+class DMTable(models.Model):
+    name = models.CharField(max_length=255)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user')
+    story = models.TextField()
+    notes = models.TextField()
+    
