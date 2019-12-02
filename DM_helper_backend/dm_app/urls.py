559bcb31fee79e727faaba7133f1c4a75ec1a9c3
jordywestside <mm1jwest@gmail.com>
2019-12-02 12:28:39 -0600

    John and Jordon User Model up and running

---
+++ DM_helper_backend/dm_app/urls.py
@@ -0,0 +1,8 @@
+
+from django.urls import path, include
+from .views import UserViewSet
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register(r'', UserViewSet, basename='user')
+urlpatterns = router.urls
