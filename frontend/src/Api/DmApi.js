31017a474d37ecf5b29933bac6a2ad1bdd0270a9
olj1 <john.losier1@gmail.com>
2019-12-02 16:37:53 -0600

    changes made at master API calls created after line 30, untested, marked as such

--- frontend/src/Api/DmApi.js
+++ frontend/src/Api/DmApi.js
@@ -1,9 +1,13 @@
+const url = "https://localhost:8000/"
+
+
 const fetchAllDM = async () => {
   let users = await fetch(`http://localhost:8000/users/`)
-  .then(res => res.json())
-  .then(data => data)
+    .then(res => res.json())
+    .then(data => data)
   return users
 }
+
 const fetchNewUser = (newUserObject) => {
   return fetch('http://localhost:8000/users/', {
     headers: {
@@ -13,18 +17,66 @@ const fetchNewUser = (newUserObject) => {
     body: JSON.stringify(newUserObject)
   })
 }
+
 const fetchEditDM = async (userid, newUserObject) => {
-  return fetch(`http://localhost:8000/users/${userid+1}/`, {
+  return fetch(`http://localhost:8000/users/${userid + 1}/`, {
     headers: {
       'Content-Type': 'application/json'
     },
-    method: 'EDIT',
+    method: 'PUT',
     body: JSON.stringify(newUserObject)
   })
 }
+
+const fetchDMTables = () => {
+  return fetch(url + "dm_tables")
+  .then((response) => response.json())
+}
+
+const fetchDMTableByID = (DMTableId) => {
+  return fetch(`${url}dm_tables/${DMTableId}`)
+  .then(res => res.json())
+  .then(data => data)
+  .catch(err => {
+    return err
+  })
+}
+
+const deleteDMTable = (DMTable) => {
+  return fetchDMTables(`${url}dm_tables/${DMTable}/`, {
+    method: `delete`
+  })
+}
+
+const addDMTable = (DMTableObject) => {
+    return fetch(`${url}/dm_tables/`, {
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      method: "POST",
+      body: JSON.stringify(DMTableObject)
+    })
+  }
+
+  const updateDMTable = (DMTableID, data) => {
+    return fetch(`${url}/dm_tables/${DMTableID}/`, {
+      method: 'PUT',
+      body: JSON.stringify(data),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }).then(res => {
+      return res;
+  }).catch(err => err);
+  }
+
 export default {
   fetchAllDM: fetchAllDM,
   fetchEditDM: fetchEditDM,
   fetchNewUser: fetchNewUser,
-
+  fetchDMTables: fetchDMTables,
+  fetchDMTableByID: fetchDMTableByID,
+  deleteDMTable: deleteDMTable,
+  addDMTable: addDMTable,
+  updateDMTable: updateDMTable,
 }
