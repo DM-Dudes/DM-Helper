e1fd5b5b5f074c8a93a52eba63ca1d0cc9930eb4
jordywestside <mm1jwest@gmail.com>
2019-12-03 15:55:07 -0600

    its fetching

--- frontend/src/Api/DmApi.js
+++ frontend/src/Api/DmApi.js
@@ -1,21 +1,15 @@
-const url = "http://localhost:8000/"
-
+const url = "https://salty-refuge-50932.herokuapp.com/https://dm-dudes-backend-api.herokuapp.com/"
 
 // DMs/Users
 const fetchAllDM = async () => {
-  let users = await fetch(`http://localhost:8000/users/`)
-<<<<<<< HEAD
-    .then(res => res.json())
-    .then(data => data)
-=======
+  let users = await fetch(`${url}users/`)
   .then(res => res.json())
   .then(data => data)
->>>>>>> 5b3a52da18abd0732ca459b8eb7562c5d4278ef7
   return users
 }
 
 const fetchNewUser = (newUserObject) => {
-  return fetch('http://localhost:8000/users/', {
+  return fetch(`${url}users/`, {
     headers: {
       'Content-Type': 'application/json'
     },
@@ -25,11 +19,7 @@ const fetchNewUser = (newUserObject) => {
 }
 
 const fetchEditDM = async (userid, newUserObject) => {
-<<<<<<< HEAD
-  return fetch(`http://localhost:8000/users/${userid + 1}/`, {
-=======
-  return fetch(`http://localhost:8000/users/${userid+1}/`, {
->>>>>>> 5b3a52da18abd0732ca459b8eb7562c5d4278ef7
+  return fetch(`${url}/users/${userid}/`, {
     headers: {
       'Content-Type': 'application/json'
     },
