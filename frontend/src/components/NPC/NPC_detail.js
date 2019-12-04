83134f2c010fb9a5fdce7ce339ccc727b9ef3095
olj1 <john.losier1@gmail.com>
2019-12-04 17:23:40 -0600

    added back button to npc detail page that redirects to table detail page based on tableid.

--- frontend/src/components/NPC/NPC_detail.js
+++ frontend/src/components/NPC/NPC_detail.js
@@ -1,25 +1,45 @@
-import React, { useState, useEffect } from 'react';
+import React, { setState, useState, useEffect } from 'react';
+import { Redirect } from "react-router-dom"
 import DmAPI from '../../Api/DmApi.js'
 
 export const NPC_detail = (props) => {
- const [NPC, setNPC] = useState(0)
-  useEffect (() => {
-   let npcID = props.npcid
-   DmAPI.fetchNPCByID(npcID)
-   .then((apiResponseJSON) => {
-         setNPC(apiResponseJSON)
-       }
-   )
- }, [])
-    return (
+  const { tableid } = props /* John */
+
+  const [backToTableDetailButton, setBackToTableDetailButton] = useState(null) /* John */
+
+  const [NPC, setNPC] = useState(0)
+
+  useEffect(() => {
+    let npcID = props.npcid
+    DmAPI.fetchNPCByID(npcID)
+      .then((apiResponseJSON) => {
+        setNPC(apiResponseJSON)
+      }
+      )
+  }, [])
+
+  const backToTableDetailonClickHandler = () => {  /* John */
+    return setBackToTableDetailButton(<Redirect to={`/table-detail/${tableid}`} />)
+  }
+  if (backToTableDetailButton) {
+    return backToTableDetailButton
+  } else {
+  return (
+    <div>
       <div>
-        Name = {NPC.name} 
-        table = {NPC.dmtable} 
-        HP = {NPC.hp} 
-        AC = {NPC.ac} 
+        Name = {NPC.name}
+        table = {NPC.dmtable}
+        HP = {NPC.hp}
+        AC = {NPC.ac}
         Details = {NPC.details}
       </div>
-    );
-  }
+      <br />
+      <div>
+        <button onClick={() => backToTableDetailonClickHandler()} name="back">Back to Table</button>
+      </div>
+    </div>
 
+  );
+}
+}
 export default NPC_detail;
