e30ef0f0f67e65de8508d89fc694246c3c43c79c
olj1 <john.losier1@gmail.com>
2019-12-04 15:19:46 -0600

    working on buttons for dmtable detail page

--- frontend/src/components/dmtable/DmTableDetail.js
+++ frontend/src/components/dmtable/DmTableDetail.js
@@ -1,9 +1,18 @@
-import React from 'react';
 import NPC_list from '../NPC/NPC_list';
+import React, { Fragment } from "react";
+import { MDBBtn, MDBIcon } from "mdbreact";
+
+
+
 
 
 const DmTableDetail = (props) => {
   const { name, userdmtable, story, notes, dmtable_id } = props
+
+  addPlayerOnClickHandler = event => {
+    
+  }
+
 
   return (
     <div>
@@ -16,6 +25,16 @@ const DmTableDetail = (props) => {
       <p>
         {notes}
       </p>
+      <div>
+        <Fragment>
+          <MDBBtn onClick= color="default">
+            Add NPC <MDBIcon icon="plus" className="ml-1" />
+          </MDBBtn>
+          <MDBBtn color="default">
+            Add Player <MDBIcon icon="plus" className="ml-1" />
+          </MDBBtn>
+        </Fragment>
+      </div>
       <div>
         <NPC_list tableid={dmtable_id} />
       </div>
