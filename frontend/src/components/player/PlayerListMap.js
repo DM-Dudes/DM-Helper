fbe730e93c8fd5834da37ddca002cce4c7aa544b
mjblaha22 <mjblaha22@gmail.com>
2019-12-04 16:47:12 -0600

    update

--- frontend/src/components/player/PlayerListMap.js
+++ frontend/src/components/player/PlayerListMap.js
@@ -3,7 +3,6 @@ import PlayerView from './Playerview.js';
 
 
 function PlayerListMap (props) {
-<<<<<<< HEAD
   console.log("props ",props.player) 
   const mapObject = () => {
     let players = props.player
@@ -26,22 +25,6 @@ function PlayerListMap (props) {
       <div>
         {() => mapObject}
       
-=======
-    
-  // const mapObject = (props, id) => {
-  //   return <PlayerView
-  //     id = {id}
-  //     name={props.name}
-  //     details={props.details}
-  //     table_id={props.dmtable}
-  //   />
-  //   }
-  
-  if (props) {
-    return (
-      <div>
-        {/* {mapObject()} */}
->>>>>>> 79d0e4b6609c0351a50b3f54d121667c27038057
       </div>
     )
     } else {
--- frontend/src/components/player/PlayerListTable.js
+++ frontend/src/components/player/PlayerListTable.js
@@ -2,18 +2,25 @@ import React, { useState, useEffect } from 'react';
 import DmAPI from '../../Api/DmApi.js'
 import PlayerListMap from './PlayerListMap.js'
 import { stat } from 'fs';
+import { Redirect } from 'react-router-dom'
  
  const PlayerListTable = (_props) => {
 
   const [players, setPlayers] = useState(0)
- 
+  const [redirect, setRedirect] = useState(false)
+  const [PlayerLink, setPlayerLink] = useState(null)
+  const handleClick = (Player_id) => {
+    console.log(Player_id)
+    setRedirect(true)
+    setPlayerLink(Player_id)
+  }
   const playerList = async () => {
     let playerId = 2
     let stateArray = []
     const playerSet = await DmAPI.fetchPlayers()
       for(let player of playerSet){
         if(player.dmtable === playerId){
-          stateArray.push(<div></div>player)
+          stateArray.push(<div>{player.name} <button onClick={() => handleClick(player.player_id)}>click me to go!</button></div>)
         }
       }
       if (players === 0){
@@ -23,16 +30,16 @@ import { stat } from 'fs';
   useEffect(() => {
     playerList()
   })
-  if (!players) {
+  if(!redirect){
     return (
-      <div>Jordan</div>
+      <div>
+        {players}
+      </div>
+    );
+  }else{
+    return(
+      <Redirect to={`/Player-detail/${PlayerLink}`}/>
     )
-  } else {
-     return (
-       <div>
-         <PlayerListMap player={players} />
-       </div>
-     )
-   }
+  }
   }
  export default PlayerListTable
