53dc00ae0048b20e70e06efb11cd04c3da9e06bf
jordywestside <mm1jwest@gmail.com>
2019-12-04 18:46:40 -0600

    jordons updates

--- frontend/src/components/NPC/NPC_detail.js
+++ frontend/src/components/NPC/NPC_detail.js
@@ -1,17 +1,32 @@
 import React, { useState, useEffect } from 'react';
 import DmAPI from '../../Api/DmApi.js'
+import { Redirect } from 'react-router-dom'
+
 
 export const NPC_detail = (props) => {
- const [NPC, setNPC] = useState(0)
+ const [NPC, setNPC] = useState('loading')
+ const [deleteDirect, setDeleteDirect] = useState(null)
  const npcID = window.location.href.substring(window.location.href.lastIndexOf('/') + 1)
-
+  console.log(NPC)
   useEffect (() => {
    DmAPI.fetchNPCByID(npcID)
    .then((apiResponseJSON) => {
          setNPC(apiResponseJSON)
        }
    )
- }, [])
+  }, [])
+
+  const handleDelete = async (id) => {
+    console.log(id)
+    let response = await DmAPI.deleteNPC(id)
+    if (response.status === 201) {
+      setDeleteDirect(true)
+  }}
+  if (deleteDirect){
+    return(
+      <Redirect to={`/table-detail/${NPC.dmtable}`}/>
+    )
+  }else{
     return (
       <div>
         Name = {NPC.name} 
@@ -19,8 +34,12 @@ export const NPC_detail = (props) => {
         HP = {NPC.hp} 
         AC = {NPC.ac} 
         Details = {NPC.details}
+        <div>
+          <button onClick={() => handleDelete(npcID)}>KILL MEEEEEEE (this will delete the NPC)</button>
+        </div>
       </div>
     );
   }
+}
 
 export default NPC_detail;
