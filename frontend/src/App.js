a0055990412a81e0620615af2d676a71d2b71169
jordywestside <mm1jwest@gmail.com>
2019-12-05 12:37:22 -0600

    now this

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -170,9 +170,9 @@ const App = () => {
           <div>
           <AppNav/>
           <div>
-          <Route exact path="/" component={() => <DMTableListPage userid='1'/>}/>
+          <Route exact path="/" component={() => <DMTableListPage userName={ userName }/>}/>
           </div>
-          <Route exact path="/create-table" component={() => <DMTableCreatePage userName={ userName }/>}/>
+          <Route exact path="/create-table" component={() => <DMTableCreatePage/>}/>
           <Route exact path="/table-detail/:tableid" component={() => <DMTableDetailsPage tableid='1'/>}/>
           <Route exact path="/NPC-detail/:npcid" component={() => <NPCDetailsPage npcid='1'/>}/>
           <Route exact path="/create-npc" component={() => <NPCs_create_page tableid='1'/>}/>
